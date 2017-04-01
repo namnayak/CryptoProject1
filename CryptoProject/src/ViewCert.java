041db6029cce4579defc4c67c1c19cc60fc1916d
hsandhu2 <hsandhu2@asu.edu>
2017-04-01 16:50:42 -0700

    Just a commit

--- CryptoProject/src/ViewCert.java
+++ CryptoProject/src/ViewCert.java
@@ -14,6 +14,7 @@ import java.security.UnrecoverableEntryException;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
+import java.security.interfaces.RSAPrivateKey;
 import java.util.Enumeration;
 
 public class ViewCert {
@@ -38,38 +39,39 @@ public class ViewCert {
 	}
 
 	public static void getpriv_key() throws KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException, UnrecoverableEntryException, NoSuchProviderException {
-		FileInputStream fis = new FileInputStream("/home/namrata/Downloads/certificate/Raghupri.pfx");
+		FileInputStream fis = new FileInputStream("/home/harsh/Documents/AppliedCrypto_HW5/certificate/Raghupri.pfx");
 		char[] password="raghu".toCharArray();
 		KeyStore ks = KeyStore.getInstance("pkcs12", "SunJSSE");
 		ks.load(fis, password);
 		KeyStore.ProtectionParameter protParam = new KeyStore.PasswordProtection(password);
 		Enumeration<String> aliases = ks.aliases();
 	
-		KeyStore.PrivateKeyEntry priv_key_entry = (PrivateKeyEntry) ks.getEntry("raghunathan srinivasan", protParam);
-		PrivateKey privkey = priv_key_entry.getPrivateKey();
-		System.out.println(privkey.toString());
+		KeyStore.PrivateKeyEntry priv_key_entry = (PrivateKeyEntry) ks.getEntry(aliases.nextElement(), protParam);
+		RSAPrivateKey privkey = (RSAPrivateKey) priv_key_entry.getPrivateKey();
+		System.out.println("Modulus:"+privkey.getModulus());
+		System.out.println("Private Exponent:"+privkey.getPrivateExponent());
 	}
 
 	public static void main(String[] args) {
 		try {
 			InputStream fis = new FileInputStream(
-					"/home/namrata/Downloads/certificate/Raghupub.cer");
+					"/home/harsh/Documents/AppliedCrypto_HW5/certificate/Raghupub.cer");
 			InputStream fis2 = new FileInputStream(
-					"/home/namrata/Downloads/certificate/Trustcenter.cer");
+					"/home/harsh/Documents/AppliedCrypto_HW5/certificate/Trustcenter.cer");
 			// BufferedInputStream bis = new BufferedInputStream(fis);
 			CertificateFactory cf = CertificateFactory.getInstance("X.509");
 			X509Certificate c = (X509Certificate) cf.generateCertificate(fis);
 			X509Certificate c2 = (X509Certificate) cf.generateCertificate(fis2);
-			/*System.out.println("Printing the certificate");
+			System.out.println("Printing the certificate");
 			readcertificate(c);
 			System.out.println("Printing the public key");
-			getpub_keys(c);*/
+			getpub_keys(c);
 			System.out.println("Printing the private key");
 			getpriv_key();
-			/*System.out.println("Printing the signature");
+			System.out.println("Printing the signature");
 			getsignature(c);
 			System.out.println("Printing the public key of CA");
-			getpub_keys(c2);*/
+			getpub_keys(c2);
 			
 		} catch (CertificateException | KeyStoreException | NoSuchAlgorithmException | UnrecoverableEntryException | IOException | NoSuchProviderException e) {
 			// TODO Auto-generated catch block
